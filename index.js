9501f3a934596b8d4215b34d9b69a3e6383e7004
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2021-12-10 21:42:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = 3000;
 
 var api_routes = require('./backendRoutes.js');
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/api', api_routes);
 
 app.use('/demo', express.static('frontendFiles'));
